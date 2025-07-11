ac702061b04f85b35230fd4080a658403b8ccd4d
Natsummerance <119987085+Natsummerance@users.noreply.github.com>
2025-07-11 12:54:42 +0800

    Add files via upload

--- app/client-root.tsx
+++ app/client-root.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { UserProvider } from "@/components/user-context";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function ClientRoot({ children }: { children: React.ReactNode }) {
-  return <UserProvider>{children}</UserProvider>;
+  return (
+    <UserProvider>
+      {children}
+      <Toaster />
+    </UserProvider>
+  );
 } 
