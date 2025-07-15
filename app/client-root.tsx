5bec07aeb9a00a47b5c3c1676d513b94e3e1894f
faluo0 <141547923+faluo0@users.noreply.github.com>
2025-07-15 18:46:22 +0800

    系统设置界面

--- app/client-root.tsx
+++ app/client-root.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { UserProvider } from "@/components/user-context";
 import { Toaster } from "@/components/ui/sonner";
+import '../lib/i18n';
 
 export default function ClientRoot({ children }: { children: React.ReactNode }) {
   return (
